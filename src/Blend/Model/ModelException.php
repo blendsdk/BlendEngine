fe10763a7aad2e2adea1722ae45e7557cc596278
Gevik Babakhani <gevik@blendjs.com>
2015-08-20 11:30:03 +0200

    Added specialized exception that is throws by a Model class

---
+++ src/Blend/Model/ModelException.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the BlendEngine framework.
+ *
+ * (c) Gevik Babakhani <gevikb@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Blend\Model;
+
+/**
+ * ModelException
+ *
+ * @author Gevik Babakhani <gevikb@gmail.com>
+ */
+class ModelException extends \Exception {
+
+    public function __construct($message) {
+        parent::__construct($message, 500, null);
+    }
+
+}
