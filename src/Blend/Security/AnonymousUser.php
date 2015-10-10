eacdf331d21382ea06066edee8ed2a5ca9563e4c
Gevik Babakhani <gevik@blendjs.com>
2015-10-10 05:52:45 +0200

    Added the AnonymousUser class

---
+++ src/Blend/Security/AnonymousUser.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the BlendEngine framework.
+ *
+ * (c) Gevik Babakhani <gevikb@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Blend\Security;
+
+use Blend\Security\IUser;
+
+/**
+ * Provides an AnonymousUser for the application
+ *
+ * @author Gevik Babakhani <gevikb@gmail.com>
+ */
+class AnonymousUser implements IUser {
+
+    public function getIdentifier() {
+        return null;
+    }
+
+    public function getUsername() {
+        return null;
+    }
+
+    public function isAuthenticated() {
+        return false;
+    }
+
+}
