528614f839f15cc70528cbdf088830df80dd5c43
Gevik Babakhani <gevik@blendjs.com>
2015-10-15 20:36:01 +0200

    Added basic user creation functionality

--- src/Blend/Service/UserManager/Database/UserManagerService.php
+++ src/Blend/Service/UserManager/Database/UserManagerService.php
@@ -12,30 +12,49 @@
 namespace Blend\Service\UserManager\Database;
 
 use Blend\Database\Database;
-use Blend\Service\UserManager\Database\SysUser;
+use Blend\Database\Model\SysUser;
 use Blend\Service\UserManager\UserManagerService as UserManagerServiceBase;
 use Blend\Service\UserManager\Database\SelectUserByTokenHashStatement;
 use Blend\Service\UserManager\Database\SelectUserByEmailStatement;
+use Blend\Service\UserManager\Database\CreateNewUserStatement;
 
 class UserManagerService extends UserManagerServiceBase {
 
-    static $USER_CLASS = 'Blend\Service\UserManager\Database\SysUser';
+    static $USER_CLASS = 'Blend\Database\Model\SysUser';
 
     public function __construct(Database $database) {
         parent::__construct($database);
     }
 
+    /**
+     * Creates a new User
+     * @param string $username
+     * @param string $password
+     * @param string $email
+     * @param string $is_active
+     * @return SysUser
+     */
+    public function createUser($username, $password, $email, $is_active = true) {
+        $stmt = new CreateNewUserStatement();
+        $stmt->setUsername($username);
+        $stmt->setPassword($password);
+        $stmt->setEmail($email);
+        $stmt->setIsActive($is_active);
+        $result = $this->database->executeStatement($stmt, self::RETURN_FIRST);
+        return is_null($result) ? $result : $this->createUserInstance($result);
+    }
+
     public function findUserByEmail($email) {
         $stmt = new SelectUserByEmailStatement();
         $stmt->setEmail($email);
-        $result = $this->database->executeStatement($stmt, Database::RETURN_FIRST);
+        $result = $this->database->executeStatement($stmt, self::RETURN_FIRST);
         return is_null($result) ? $result : $this->createUserInstance($result);
     }
 
     public function authenticate($token) {
         $stmt = new SelectUserByTokenHashStatement();
         $stmt->setToken($token);
-        $result = $this->database->executeStatement($stmt, Database::RETURN_FIRST);
+        $result = $this->database->executeStatement($stmt, self::RETURN_FIRST);
         return is_null($result) ? $result : $this->createUserInstance($result);
     }
 
