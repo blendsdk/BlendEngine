f4d3aac081af8704cfe5a8e563ccca3e2fb1e850
Gevik Babakhani <gevik@blendjs.com>
2016-01-29 10:34:44 +0100

    removed deprecated files

--- src/Blend/Console/templates/model.php
+++
@@ -1,9 +0,0 @@
-<?php echo "<?php\n" ?>
-
-namespace <?php echo $table->getModelNamespace() ?>;
-
-use <?php echo $table->getModelBaseNamespace() . "\\" . $table->getClassName(); ?> as Base;
-
-class <?php echo $table->getClassName(); ?> extends Base {
-
-}
