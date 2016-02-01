ac2fa2c9824f26a5f01658ed2fe8e1b79ccf8f3e
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-02-01 09:09:10 +0100

    save point query builder

---
+++ tests/Database/QueryBuilderTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Blend\Tests\Database;
+
+use Blend\Tests\Database\DatabaseTestBase;
+
+class QueryBuilderTest extends DatabaseTestBase {
+
+    public static function getTestingDatabaseConfig() {
+        return array_merge(static::getDefaultDatabaseConfig(), ['database' => 'blend_unit_test_db']);
+    }
+
+    public function testSanity() {
+        $db = $this->getTestDatabase();
+        $db->executeScalar('select 1');
+    }
+
+}
