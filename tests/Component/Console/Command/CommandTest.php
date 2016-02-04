e067cc66e51471b89eb1d493b3cc9d6c3642bbe8
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-02-04 23:45:18 +0100

    Savepoint testing commands

---
+++ tests/Component/Console/Command/CommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the BlendEngine framework.
+ *
+ * (c) Gevik Babakhani <gevikb@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Blend\Tests\Component\Console\Command;
+
+use Blend\Component\Console\Application;
+use Blend\Tests\ProjectUtil;
+use Blend\Tests\Component\Console\Command\TestCommand;
+
+/**
+ * Description of CommandTest
+ *
+ * @author Gevik Babakhani <gevikb@gmail.com>
+ */
+class CommandTest extends \PHPUnit_Framework_TestCase {
+
+    public function testSanityCommandTest() {
+
+        $projectFolder = ProjectUtil::createNewProject("CommandTest",true);
+        $app = new Application($projectFolder,'CommandTest');
+        $app->add(new TestCommand());
+        $result = ProjectUtil::runCommand($projectFolder, 'test:testcommand', [], $app);
+    }
+
+}
