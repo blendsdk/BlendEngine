55902d068a67cd2a6e851706afac8c1867411961
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-02-04 10:34:00 +0100

    Refactored the pupunit bootstrap

--- phpunit_bootstrap.php
+++ phpunit_bootstrap.php
@@ -9,17 +9,25 @@
  * file that was distributed with this source code.
  */
 
+if (!ini_get('date.timezone')) {
+    ini_set('date.timezone', 'Europe/Amsterdam');
+}
 
-$autoloadPath = null;
-$paths = array(
-    dirname(__DIR__) . '/vendor/autoload.php',
-    dirname(__DIR__) . '/../../vendor/autoload.php',
-);
-
-foreach ($paths as $path) {
-    if (file_exists($path)) {
-        $autoloadPath = $path;
+foreach (array(__DIR__ . '/../../autoload.php', __DIR__ . '/../vendor/autoload.php', __DIR__ . '/vendor/autoload.php') as $file) {
+    if (file_exists($file)) {
+        define('BLEND_COMPOSER_INSTALL', $file);
+        break;
     }
 }
 
-include $autoloadPath;
+unset($file);
+
+if (!defined('BLEND_COMPOSER_INSTALL')) {
+    fwrite(STDERR, 'You need to set up the project dependencies using the following commands:' . PHP_EOL .
+            'wget http://getcomposer.org/composer.phar' . PHP_EOL .
+            'php composer.phar install' . PHP_EOL
+    );
+    die(1);
+}
+
+require BLEND_COMPOSER_INSTALL;
