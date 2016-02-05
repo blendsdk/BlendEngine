af413e13cad1b351b9203afe9fc1404b56039f19
Gevik Babakhani <gevik@blendjs.com>
2016-02-05 12:00:43 +0100

    Enhanced composer checking

--- src/ProjectSetup/Resources/Templates/Basic/bin/app.php
+++ src/ProjectSetup/Resources/Templates/Basic/bin/app.php
@@ -1,8 +1,29 @@
 <?php print_php_header();?>
 
-include dirname(__FILE__) . '/../vendor/autoload.php';
+if (!ini_get('date.timezone')) {
+    ini_set('date.timezone', 'Europe/Amsterdam');
+}
+
+foreach (array(__DIR__ . '/../../autoload.php', __DIR__ . '/../vendor/autoload.php', __DIR__ . '/vendor/autoload.php') as $file) {
+    if (file_exists($file)) {
+        define('BLEND_COMPOSER_INSTALL', $file);
+        break;
+    }
+}
+
+unset($file);
+
+if (!defined('BLEND_COMPOSER_INSTALL')) {
+    fwrite(STDERR, 'You need to set up the project dependencies using the following commands:' . PHP_EOL .
+            'wget http://getcomposer.org/composer.phar' . PHP_EOL .
+            'php composer.phar install' . PHP_EOL
+    );
+    die(1);
+}
+
+require BLEND_COMPOSER_INSTALL;
 
 use <?php echo $applicationNamespace;?>\Console\<?php echo $applicationCommandClassName?>;
 
-$app = new <?php echo $applicationCommandClassName?>(__DIR__);
+$app = new <?php echo $applicationCommandClassName?>(__DIR__.'/../');
 $app->run();
