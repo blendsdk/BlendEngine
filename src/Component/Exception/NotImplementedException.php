89fa5af91bf545422139b8b533d031f929497d2d
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-02-07 22:43:01 +0100

    Renamed

---
+++ src/Component/Exception/NotImplementedException.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the BlendEngine framework.
+ *
+ * (c) Gevik Babakhani <gevikb@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Blend\Component\Exception;
+
+/**
+ * NotYetImplementedException represents an exception caused by calling a
+ * code block that is not implemented yet
+ *
+ * @author Gevik Babakhani <gevikb@gmail.com>
+ */
+class NotImplementedException extends \Exception {
+    
+}
