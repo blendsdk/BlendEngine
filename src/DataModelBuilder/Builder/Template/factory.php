e49fe78795e93a4a599da1759ef9d7ab83e0aa2a
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-02-10 01:06:39 +0100

    Added code doc to the factory template

--- src/DataModelBuilder/Builder/Template/factory.php
+++ src/DataModelBuilder/Builder/Template/factory.php
@@ -6,6 +6,10 @@ namespace <?php echo $appNamespace . '\\' . $classNamespace ?>;
 use <?php echo $use; ?>;
 <?php endforeach; ?>
 
+/**
+ * <?php echo $className ?> is a data factory utility class for
+ * the "<?php echo $classFQRN; ?>" relation
+ */
 <?php if (isset($classModifier)) echo $classModifier . ' '; ?>class <?php echo $className ?> extends <?php echo $classBaseClass; ?> {
 
 }
