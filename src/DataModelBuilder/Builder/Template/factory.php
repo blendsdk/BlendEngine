3432922e5fccc83ed5607b35443be7266cb4a719
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-02-12 22:45:31 +0100

    Added the convertFromModel to the factory template

--- src/DataModelBuilder/Builder/Template/factory.php
+++ src/DataModelBuilder/Builder/Template/factory.php
@@ -15,6 +15,7 @@ use <?php echo $use; ?>;
 <?php if (isset($generate)):?>
     public function __construct(Database $database) {
         parent::__construct($database, '<?php echo $modelClass?>');
+        $this->relation = '<?php echo $classFQRN?>';
 <?php if(isset($fieldConverter)):?>
         $this->fieldConverter = new <?php echo "{$fieldConverter}(['datetimeFormat' => DateTimeConversion::SETTINGS])";?>;
 <?php endif;?>
@@ -24,11 +25,29 @@ use <?php echo $use; ?>;
     protected function convertFromRecord(array $record) {
 <?php foreach($converters as $field => $converterlist):?>
 <?php foreach($converterlist as $converter):?>
-        $this->fieldConverter->fromRecord($record, '<?php echo $field;?>', '<?php echo $converter?>');
+        $this->fieldConverter->fromRecord($record, '<?php echo $field;?>', <?php echo is_string($converter) ? "'{$converter}'" :  $converter; ?>);
 <?php endforeach;?>
 <?php endforeach;?>
         return $record;
     }
+
+    protected function convertFromModel(array $data) {
+<?php foreach($converters as $field => $converterlist):?>
+<?php foreach($converterlist as $converter):?>
+    $this->fieldConverter->fromModel($data, '<?php echo $field;?>', <?php echo is_string($converter) ? "'{$converter}'" :  $converter; ?>);
+<?php endforeach;?>
+<?php endforeach;?>
+         return $data;
+    }
 <?php endif;?>
 <?php endif;?>
+
+    /**
+     * Returns a new instance of <?php echo $modelClass."\n"?>
+     * @return <?php echo $modelClass."\n"?>
+     */
+    public function newModel() {
+        return parent::newModel();
+    }
+
 }
