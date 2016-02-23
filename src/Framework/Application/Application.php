2ff46f27525918258dd8d1201193ad5df6db522b
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-02-24 00:59:53 +0100

    Better DI design and usage

--- src/Framework/Application/Application.php
+++ src/Framework/Application/Application.php
@@ -14,10 +14,9 @@ namespace Blend\Framework\Application;
 use Blend\Component\Application\Application as BaseApplication;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Blend\Component\Exception\InvalidConfigException;
+use Blend\Component\DI\Container;
+use Psr\Log\LoggerInterface;
 use Blend\Component\Configuration\Configuration;
-use Monolog\Logger;
-use Monolog\Handler\RotatingFileHandler;
 
 /**
  * Application
@@ -27,9 +26,9 @@ use Monolog\Handler\RotatingFileHandler;
 class Application extends BaseApplication {
 
     /**
-     * @var string
+     * @var Container
      */
-    protected $name;
+    protected $container;
 
     /**
      * @var string
@@ -37,32 +36,23 @@ class Application extends BaseApplication {
     protected $rootFolder;
 
     /**
-     * @var boolean
+     * @var LoggerInterface
      */
-    protected $debug;
+    protected $logger;
 
     /**
      * @var Configuration
      */
     protected $config;
 
-    /**
-     * @var Logger
-     */
-    protected $logger;
-
-    public function __construct($rootFolder) {
-        parent::__construct();
+    public function __construct(Container $container
+    , Configuration $config
+    , LoggerInterface $logger
+    , $rootFolder) {
+        $this->container = $container;
         $this->rootFolder = $rootFolder;
-    }
-
-    protected function checkGetFolder($folder) {
-        if (!is_dir($folder) && !is_writable($folder)) {
-            throw new InvalidConfigException(
-            "$folder does not exist or it is not writable"
-            , 500);
-        }
-        return $folder;
+        $this->config = $config;
+        $this->logger = $logger;
     }
 
     protected function finalize(Request $request, Response $response) {
@@ -77,86 +67,8 @@ class Application extends BaseApplication {
         
     }
 
-    /**
-     * Loads the application configuration file and caches it in the case
-     * does not exist. The configuration object (Configuration::class) is then 
-     * registered as singleton the the application container
-     * @throws InvalidConfigException
-     */
-    protected function loadConfiguration() {
-
-        $configFile = $this->rootFolder . '/config/config.php';
-        if (!file_exists($configFile)) {
-            throw new InvalidConfigException("Missing the configiration file: "
-            . $configFile, 500);
-        }
-
-        $cacheFile = $this->checkGetFolder($this->rootFolder . '/var/cache')
-                . '/config.cache';
-
-        if (file_exists($cacheFile)) {
-            $config = new Configuration();
-            $config->load($cacheFile);
-        } else {
-            $config = Configuration::createFromFile($configFile);
-            $config->dump($cacheFile);
-        }
-
-        $this->debug = $config->get('debug', $this->debug);
-        $this->config = $config;
-
-        $this->container->singleton(Configuration::class, [
-            'factory' => function() use ($config) {
-                return $config;
-            }
-        ]);
-    }
-
-    /**
-     * Overridable function to setup custom logger handlers.
-     * By default the Application will create a rotating file logger
-     * @param Logger $logger
-     * @param int $defaultLevel
-     */
-    protected function setupLoggerHandlers(Logger $logger, $defaultLevel) {
-        $this->setupRotatingFileLogger($logger, $defaultLevel);
-    }
-
-    /**
-     * Creates a rotaing file logger handler. The number of files to keep
-     * is read fon the application config ogger.filelog.maxfiles = 10
-     * @param Logger $logger
-     * @param type $level
-     */
-    protected function setupRotatingFileLogger(Logger $logger, $level) {
-
-        $logFile = $this->checkGetFolder($this->rootFolder . '/var/log')
-                . '/' . strtolower($this->name) . '.log';
-
-        $fileHandler = new RotatingFileHandler(
-                $logFile, $this->config->get('logger.filelog.maxfiles', 10), $level);
-        $logger->pushHandler($fileHandler);
-    }
-
-    /**
-     * Initializes the logger for this application
-     */
-    private function createLogger() {
-        $logger = new Logger($this->name);
-        $defaultLevel = $this->debug === false ? Logger::WARNING : Logger::DEBUG;
-        $this->setupLoggerHandlers($logger, $defaultLevel);
-        $logger->debug('Logger started!');
-
-        $this->container->singleton(Logger::class, [
-            'factory' => function() use($logger) {
-                return $logger;
-            }
-        ]);
-    }
-
     protected function initialize(Request $request) {
-        $this->loadConfiguration();
-        $this->createLogger();
+        
     }
 
 }
