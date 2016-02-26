613ff77e3e134a80e1f586dd121523947e1f07b6
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-02-26 11:03:30 +0100

    Savepoint serve command

--- src/Framework/Console/Command/ServeCommand.php
+++ src/Framework/Console/Command/ServeCommand.php
@@ -13,7 +13,6 @@ namespace Blend\Framework\Console\Command;
 
 use Blend\Framework\Console\Command\Command;
 use Symfony\Component\Process\ProcessUtils;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -24,10 +23,37 @@ use Symfony\Component\Console\Input\InputOption;
  */
 class ServeCommand extends Command {
 
+    protected $port;
+
     protected function configure() {
+        $this->port = $this->findNextAvailablePort('0.0.0.0');
         $this->setName('serve')
                 ->setDescription('Serve the application on the PHP development server')
-                ->addOption('port', null, InputOption::VALUE_OPTIONAL, 'The port to serve the application on.');
+                ->addOption('port'
+                        , 'p'
+                        , InputOption::VALUE_OPTIONAL
+                        , 'The port to serve the application on.', $this->port)
+                ->addOption('host'
+                        , 'H'
+                        , InputOption::VALUE_OPTIONAL
+                        , 'The host address to serve the application on.'
+                        , '0.0.0.0');
+    }
+
+    protected function execute(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output) {
+        $base = ProcessUtils::escapeArgument($this->getApplication()->getProjectFolder());
+        $binary = ProcessUtils::escapeArgument((new PhpExecutableFinder)->find(false));
+    }
+
+    private function findNextAvailablePort($host) {
+        for ($port = 8000; $port != 9000; $port++) {
+            $connection = @fsockopen($host, $port);
+            if (is_resource($connection)) {
+                fclose($connection);
+            } else {
+                return $port;
+            }
+        }
     }
 
 }
