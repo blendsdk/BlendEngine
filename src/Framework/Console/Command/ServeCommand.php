6706558dba0b2c133984e27104598a98bc4c93f2
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-02-26 11:18:09 +0100

    save point

--- src/Framework/Console/Command/ServeCommand.php
+++ src/Framework/Console/Command/ServeCommand.php
@@ -15,6 +15,8 @@ use Blend\Framework\Console\Command\Command;
 use Symfony\Component\Process\ProcessUtils;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Description of ServeCommand
@@ -23,10 +25,8 @@ use Symfony\Component\Console\Input\InputOption;
  */
 class ServeCommand extends Command {
 
-    protected $port;
-
     protected function configure() {
-        $this->port = $this->findNextAvailablePort('0.0.0.0');
+        $port = $this->findNextAvailablePort('0.0.0.0');
         $this->setName('serve')
                 ->setDescription('Serve the application on the PHP development server')
                 ->addOption('port'
@@ -37,14 +37,44 @@ class ServeCommand extends Command {
                         , 'H'
                         , InputOption::VALUE_OPTIONAL
                         , 'The host address to serve the application on.'
-                        , '0.0.0.0');
+                        , '0.0.0.0')
+                ->addOption('name'
+                        , 'n'
+                        , InputOption::VALUE_OPTIONAL
+                        , 'Name of the application to run'
+                        , strtolower($this->getConfig('name')));
     }
 
-    protected function execute(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output) {
+    protected function execute(InputInterface $input, OutputInterface $output) {
         $base = ProcessUtils::escapeArgument($this->getApplication()->getProjectFolder());
         $binary = ProcessUtils::escapeArgument((new PhpExecutableFinder)->find(false));
+
+        if (($scriptName = $this->findApplication($input->getOption('name'))) !== false) {
+            $host = $input->getOption('host');
+            $port = $input->getOption('port');
+            $output->writeln("<warn>Running the application on $host:$port</warn>");
+        } else {
+            $output->writeln("<error>Unable to find the $scriptName!");
+        }
     }
 
+    private function findApplication($name) {
+        $sctipyName = $name . '_dev.php';
+        $file = $this->getApplication()->getProjectFolder()
+                . '/web/'
+                . $sctipyName;
+        if (file_exists($sctipyName)) {
+            return $sctipyName;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Find the next open port
+     * @param type $host
+     * @return int
+     */
     private function findNextAvailablePort($host) {
         for ($port = 8000; $port != 9000; $port++) {
             $connection = @fsockopen($host, $port);
