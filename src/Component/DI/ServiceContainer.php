3623060e4514b1c19a2927fe46276a8b016f549c
Gevik Babakhani <gevikb@gmail.com>
2016-03-07 08:55:34 -0500

    Implemented option to dynamically load an "externally" namespaced module

--- src/Component/DI/ServiceContainer.php
+++ src/Component/DI/ServiceContainer.php
@@ -12,6 +12,7 @@
 namespace Blend\Component\DI;
 
 use Blend\Component\DI\Container;
+use Composer\Autoload\ClassLoader;
 
 /**
  * ServiceContainer
@@ -27,17 +28,41 @@ class ServiceContainer extends Container {
      * @return boolean
      */
     public function loadServicesFromFile($filename) {
+        $classLoader = $this->getClassLoader();
         if (file_exists($filename)) {
             $config = json_decode(file_get_contents($filename), true);
             foreach ($config as $interface => $serviceDescription) {
                 if (is_string($serviceDescription)) {
                     $this->defineSingletonWithInterface($interface
                             , $serviceDescription);
+                } else if ($classLoader !== null &&
+                        is_array($serviceDescription) &&
+                        count($serviceDescription) == 2) {
+                    list($folder, $className) = $serviceDescription;
+                    $ns = explode('\\', $className);
+                    $classLoader->addPsr4($ns[0] . '\\', $folder);
+                    $this->defineSingletonWithInterface($interface
+                            , $className);
                 }
             }
             return true;
         }
         return false;
+    }
+
+    /**
+     * @return ClassLoader
+     */
+    private function getClassLoader() {
+        $category = spl_autoload_functions();
+        foreach ($category as $splLoaders) {
+            foreach ($splLoaders as $loader) {
+                if ($loader instanceof ClassLoader) {
+                    return $loader;
+                }
+            }
+        }
+        return null;
     }
 
 }
