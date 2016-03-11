bf3b627c2866edd3691d917002354044490046d7
Gevik Babakhani <gevikb@gmail.com>
2016-03-11 06:54:00 -0500

    Added the missing ServicesSyncCommand

--- src/ProjectSetup/Resources/Templates/Basic/src/Console/Application.php
+++ src/ProjectSetup/Resources/Templates/Basic/src/Console/Application.php
@@ -5,6 +5,7 @@ namespace <?php echo $applicationNamespace;?>\Console;
 use Blend\Framework\Console\Application;
 use Blend\Framework\Console\Command\ServeCommand;
 use Blend\DataModelBuilder\Command\DataModelCommand;
+use Blend\Framework\Console\Command\ServicesSyncCommand;
 
 /**
  * Description of ApplicationCommand
@@ -16,6 +17,7 @@ class <?php echo $applicationCommandClassName;?> extends Application {
     public function __construct($scriptPath) {
         parent::__construct($scriptPath, '<?php echo $applicationName?> Command Utility', '1.0');
         $this->add(new ServeCommand());
+        $this->add(new ServicesSyncCommand());
         $this->add(new DataModelCommand());
     }
 
