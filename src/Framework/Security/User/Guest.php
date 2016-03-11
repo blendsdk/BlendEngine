b1c73057ad7ec41870d02f6d8ef110fa8c92cab3
Gevik Babakhani <gevikb@gmail.com>
2016-03-11 08:58:27 -0500

    Added a Guest user

---
+++ src/Framework/Security/User/Guest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the BlendEngine framework.
+ *
+ * (c) Gevik Babakhani <gevikb@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Blend\Framework\Security\User;
+use Blend\Framework\Security\User\UserProviderInterface;
+
+/**
+ * Guest
+ *
+ * @author Gevik Babakhani <gevikb@gmail.com>
+ */
+class Guest implements UserProviderInterface {
+
+    public function getEmail() {
+        return null;
+    }
+
+    public function getPermissions() {
+        return [];
+    }
+
+    public function getRoles() {
+        return [];
+    }
+
+    public function getUserID() {
+        return null;
+    }
+
+    public function getUsername() {
+        return null;
+    }
+
+    public function hasPermission($permission) {
+        return false;
+    }
+
+    public function hasRole($role) {
+        return false;
+    }
+
+    public function isActive() {
+        return false;
+    }
+
+    public function isGuest() {
+        return true;
+    }
+
+}
