0e7ea3ebf0f6341d17e263610f086bb321a64d54
Gevik Babakhani <gevikb@gmail.com>
2016-03-18 05:49:33 -0400

    Fixed typo

--- src/ProjectSetup/Command/InitCommand.php
+++ src/ProjectSetup/Command/InitCommand.php
@@ -127,7 +127,7 @@ class InitCommand extends Command {
         extract($this->renderContext);
 
         $this->renameTable = array(
-            'src/gitignore' => '.gitignore',
+            'src/gitignore' => 'src/.gitignore',
             'config/gitignore' => 'config/.gitignore',
             'resources/gitignore' => 'resources/.gitignore',
             'resources/sass/app.scss' => 'resources/sass/' . $applicationScriptName . '.scss',
