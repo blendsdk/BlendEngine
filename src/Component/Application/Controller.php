74c1923cedb01dc82e694debe166fd2e0d5048d1
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-09-02 09:19:53 +0200

    Added a base controller

---
+++ src/Component/Application/Controller.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the BlendEngine framework.
+ *
+ * (c) Gevik Babakhani <gevikb@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Blend\Component\Application;
+
+use Blend\Component\Templating\EngineInterface;
+use Blend\Framework\Support\Runtime\RuntimeProviderInterface;
+
+/**
+ * Controller
+ *
+ * @author Gevik Babakhani <gevikb@gmail.com>
+ */
+abstract class Controller {
+
+    /**
+     * @var EngineInterface
+     */
+    protected $renderer;
+
+    /**
+     * @var RuntimeProviderInterface;
+     */
+    protected $runtime;
+
+    protected abstract function getTemplatesFolder();
+
+    public function __construct(EngineInterface $renderer, RuntimeProviderInterface $runtime) {
+        $this->renderer = $renderer;
+        $this->runtime = $runtime;
+        $this->renderer->setViewPaths([
+            $this->getTemplatesFolder()
+        ]);
+    }
+
+}
